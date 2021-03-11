aa8be70117bffb462322f6d3ba29aca959158cc8
sushilnitkkr <raj33698@gmail.com>
2021-03-11 16:30:31 +0530

    Add index to import all components nicely

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import {Cards, Chart, CountryPicker} from './components'; 
 
 class App extends React.Component{
     render(){
         return (
             <div>
-                <h1>App</h1>
+               <Cards />
+               <Chart />
+               <CountryPicker />
             </div>
         )
     }
